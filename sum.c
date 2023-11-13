ab9c12f4fa2c14bb31867d9d61f64cf5eb03d043
johnmuthua <caveomuthama@gmail.com>
2023-11-13 05:41:36 +0300

    Used a while loop to check process_id

--- sum.c
+++ sum.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main ()
+void print_id()
 {
 	printf("Function with the process id %d begins\n",getpid());
 	printf("The parent is  %d begins\n",getppid());
+}
+int main ()
+{
+	int x;
+	x = 10;
+	while (x > 0)
+	{
+		print_id();
+		x--;
+	}
+	return (0);
 }
