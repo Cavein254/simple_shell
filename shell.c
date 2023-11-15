9185082f53519d6311e86ef618671a90fd4dff94
johnmuthua <caveomuthama@gmail.com>
2023-11-15 23:52:28 +0300

    Add argument list

--- shell.c
+++ shell.c
@@ -1,16 +1,32 @@
 #include "c_shell.h"
 
 int main(void) {
-  char *input_addr;
+  char *commands = NULL, *delimiter = NULL, *commands_token = NULL;
+  char *cpy_commands = NULL;
+  char **list_args = NULL;
   size_t buf_size;
+  int counter = 0;
   char prompt[] = "#cisfun$ ";
 
-  input_addr = NULL;
+  delimiter = " \n";
   buf_size = 0;
 
   while (true) {
     write(1, prompt, sizeof(prompt) - 1);
-    getline(&input_addr, &buf_size, stdin);
+    if (getline(&commands, &buf_size, stdin) == -1)
+      return (-1);
+
+    /*Make a copy of commands since strtok modifies original string*/
+    cpy_commands = strdup(commands);
+    commands_token = strtok(commands, delimiter);
+
+    while (commands_token != '\n') {
+      list_args[counter] = commands_token;
+      commands_token = strtok(NULL, delimiter);
+      counter++;
+    }
+    /*All args end with a NULL*/
+    list_args[counter] = NULL;
   }
   return (0);
 }
